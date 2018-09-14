231e4a7c10de3eb6f64e2e59d745dc073794d542
wcx <>
2018-09-14 09:09:00 +0800

    modify xxx

--- python_Functional_2.py
+++ python_Functional_2.py
@@ -99,16 +99,42 @@ def lazy_sum(*args):
 
 # Python内置的functools.wraps就是干这个事,转化func的name
 
-def log(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kw):
-        print('call %s():' % func.__name__)
-        return func(*args, **kw)
-    return wrapper
+# def log(func):
+#     @functools.wraps(func)
+#     def wrapper(*args, **kw):
+#         print('call %s():' % func.__name__)
+#         return func(*args, **kw)
+#     return wrapper
 
-@log
-def now():
-     print('2018-09-11')
+# @log
+# def now():
+#      print('2018-09-11')
 
-now = log(now)
-print(now.__name__) # wrapper
+# now = log(now)
+# print(now.__name__) # now
+
+## 偏函数
+
+# int()函数还提供额外的base参数，默认值为10。如果传入base参数，就可以做N进制的转换：
+
+int('123', base = 2)
+
+# 假设要转换大量的二进制字符串，每次都传入int(x, base=2)非常麻烦，于是，我们想到，可以定义一个int2()的函数，默认把base=2传进去：
+
+def int2(x, base = 2):
+    return int(x, base)
+
+# functools.partial就是帮助我们创建一个偏函数的，不需要我们自己定义int2()，可以直接使用下面的代码创建一个新的函数int2：
+
+int22 = functools.partial(int, base=2)
+
+# 所以，简单总结functools.partial的作用就是，把一个函数的某些参数给固定住（也就是设置默认值），返回一个新的函数，调用这个新函数会更简单。
+# 注意到上面的新的int22函数，仅仅是把base参数重新设定默认值为2，但也可以在函数调用时传入其他值：
+int22('10000', base = 10)
+
+# 创建偏函数时，实际上可以接收函数对象、*args和**kw这3个参数
+
+# 当传入 max2 = functools.partial(max, 10) 实际上会把10作为*args的一部分自动加到左边
+# 也就是 max2(5, 6, 7)  相当于 args = (10, 5, 6, 7) max(*args) 结果为10
+
+# 当函数的参数个数太多，需要简化时，使用functools.partial可以创建一个新的函数，这个新函数可以固定住原函数的部分参数，从而在调用时更简单
---
+++ python_module.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+## 作用域
+# 之所以我们说，private函数和变量“不应该”被直接引用，而不是“不能”被直接引用，是因为Python并没有一种方法可以完全限制访问private函数或变量，但是，从编程习惯上不应该引用private函数或变量。
+# 我们在模块里公开greeting()函数，而把内部逻辑用private函数隐藏起来了，这样，调用greeting()函数不用关心内部的private函数细节，这也是一种非常有用的代码封装和抽象的方法，即：
+#外部不需要引用的函数全部定义成private，只有外部需要引用的函数才定义为public。
+
+def _private_1(name):
+    return 'Hello, %s' % name
+
+def _private_2(name):
+    return 'Hi, %s' % name
+
+def greeting(name):
+    if len(name) > 3:
+        return _private_1(name)
+    else:
+        return _private_2(name)
