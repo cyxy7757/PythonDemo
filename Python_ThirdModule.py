31cda1af2728fac0f10497cf8aec9718dfbb69d9
wcx <wcx0020@163.com>
2018-09-19 22:30:58 +0800

    add email post

--- Python_ThirdModule.py
+++ Python_ThirdModule.py
@@ -1,4 +1,9 @@
 # 常用第三方模块
 import requests
 r = requests.get('https://www.douban.com/') # 豆瓣首页
-print(r.status_code)
+# print(r.status_code)
+# print(r.headers)
+
+import psutil
+print(psutil.cpu_count())
+
